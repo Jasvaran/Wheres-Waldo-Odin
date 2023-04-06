026279988b768890fcebce2817844a311a06b7fe
Jasvaran Mann <mannjesse355@gmail.com>
2023-04-05 23:09:18 -0700

    added selection validation when selecting a character

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,6 +7,7 @@ import { getAuth } from "firebase/auth";
 import img1 from '../../assets/wheres-waldo.jpeg'
 import useMousePosition from "../hooks/useMousePosition";
 import DropDownMenu from "../dopDownMenu/DropDownMenu";
+import { getFirestore, doc, getDoc, collection, setDoc } from 'firebase/firestore'
 
 export default function Main(props) {
 
@@ -14,10 +15,40 @@ export default function Main(props) {
     const [authData, setAuthData] = useState({})
     const [coords, setCoords] = useState({x: null, y: null});
     const [hide, setHide] = useState(null)
+    const [region, setRegion] = useState(null)
+    const [selection, setSelection] = useState(null)
 
     const mousePos = useMousePosition();
 
     let navigate = useNavigate();
+
+    async function validateSelection(){
+
+        try {
+            const db = getFirestore();
+            const charRef = doc(db, 'characters', 'odin')
+            // await setDoc(doc(charRef, 'two'), {'two': 'michael'})
+            const charSnap = await getDoc(charRef)
+            let data = charSnap.data()
+            console.log(data)
+            if (region === 'hitbox' && selection === data.one){
+                alert('correct')
+            } else {
+                alert('incorrect')
+            }
+
+        } catch {
+            console.log('undefined')
+        }
+
+
+        }
+        
+    
+
+
+
+    
 
     useEffect(() => {
         let authToken = sessionStorage.getItem('Auth Token')
@@ -32,28 +63,43 @@ export default function Main(props) {
     }, [])
 
     useEffect(() => {
-        triggerWaldo()
+        triggerMenu()
     }, [mousePos])
    
-    function triggerWaldo(e){
+    function triggerMenu(e){
         setHide('visible')
         setCoords({x: mousePos.x, y: mousePos.y})
+        
 
     }
 
-    function hideCallback(){
+    useEffect(() => {
+        validateSelection()
+    }, [selection])
+
+    async function hideCallback(selection){
         setHide('hidden')
+        setSelection(selection)
+        
+        console.log('sel', selection)
+        console.log('region', region)
     }
 
+    function checkRegion(e){
+        setRegion(e.target.id)
+
+    }
     return (
         <div className="main">
             <Nav signOut={props.signOut} name={props.name} />
             <h1 className="title">Wheres Waldo</h1>
-            <div className="waldo-container">
-                <img className="waldo" id="waldo" src={img1} alt=""></img>
-            {<DropDownMenu x={coords.x} y={coords.y} hide={hide} hideCallback={hideCallback} />}
+            <div className="waldo-container" onClick={checkRegion}>
+                <img className="waldo" id="waldo-img" src={img1} alt=""></img>
+                <div id="hitbox">X</div>
             </div>
+            {<DropDownMenu x={coords.x} y={coords.y} hide={hide} hideCallback={hideCallback} />}
             {JSON.stringify(mousePos)}
+
         </div>
     )
 }
