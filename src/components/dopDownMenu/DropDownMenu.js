bac2a6c9eb519735f2815363c8d5bfda2fe762a8
Jasvaran Mann <mannjesse355@gmail.com>
2023-04-05 23:10:25 -0700

    replaced section ele with div

--- src/components/dopDownMenu/DropDownMenu.js
+++ src/components/dopDownMenu/DropDownMenu.js
@@ -4,17 +4,18 @@ export default function DropDownMenu(props){
 
 
 
-    function hideMenu(){
-        props.hideCallback();
+    function hideMenu(e){
+        console.log(e)
+        props.hideCallback(e.target.id);
     }
 
     return (
         <div className="drop-down-menu" style={{'position': 'absolute', 'top': props.y, 'left': props.x, 'visibility': props.hide }}>
-            <select onInput={hideMenu}>
-                <option>Waldo</option>
-                <option>Michael</option>
-                <option>James</option>
-            </select>
+            <div>
+                <p id="michael" onClick={hideMenu}>michael</p>
+                <p id="waldo" onClick={hideMenu}>waldo</p>
+                <p id="james" onClick={hideMenu}>james</p>
+            </div>
         </div>
     )
 }
